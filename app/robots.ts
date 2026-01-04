d1a5c28f4043ffe0c4a2a7ed744914f245b38375
Kadek Rista Dwipurnamii <ristadwipurn@gmail.com>
2026-01-04 10:53:30 +0800

    mengedit robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 export default function robots() {
   return {
     rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: "https://www.fourwander.my.id/sitemap.xml",
+    sitemap: "https://fourwander.my.id/sitemap.xml",
   };
 }
