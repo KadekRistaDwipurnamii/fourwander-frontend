c60831af1e6175b47d1827fc2ef4233719eabd2c
Kadek Rista Dwipurnamii <ristadwipurn@gmail.com>
2026-01-04 12:42:44 +0800

    edit page.tsx

--- app/paket/[slug]/page.tsx
+++ app/paket/[slug]/page.tsx
@@ -28,7 +28,7 @@ export default function PaketDetailPage() {
   const [jumlahOrang, setJumlahOrang] = useState(1);
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/paket/${slug}`)
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/paket/slug/${slug}`)
       .then(res => res.json())
       .then(data => {
         setPaket({
